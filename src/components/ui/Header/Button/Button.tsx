3e6e669a8adf5f85b1913fc981dff61dc8297682
makaricheva liza <makarichevalm@yandex.ru>
2024-11-24 17:56:20 +0300

    add button

---
+++ src/components/ui/Header/Button/Button.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import './Button.scss'
+
+type TButton = {
+  name:string
+}
+const Button:FC<TButton> = ({name}) => {
+  return (
+   <button className='blueButton'>{name}</button>
+  )
+}
+
+export default Button
