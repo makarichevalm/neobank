fb241e9cc221c9262b53c84be249c05020236927
makaricheva liza <makarichevalm@yandex.ru>
2024-12-10 23:19:27 +0300

    some changes in news card

--- src/components/ui/NewsCard/NewsCard.tsx
+++ src/components/ui/NewsCard/NewsCard.tsx
@@ -1,21 +1,15 @@
 import { FC } from 'react'
 import './NewsCard.scss'
+import { TNewsItem } from '../../../types/index.ts'
 
-type TCard = {
-  urlToImage: string
-  title: string
-  url: string
-  description: string
-}
-
-const NewsCard: FC<TCard> = ({ urlToImage, title, url, description }) => {
+const NewsCard: FC<TNewsItem> = ({ urlToImage, title, url, description }) => {
   return (
     <a className='newsCard' href={url} target='_blank'>
       <div>
-        <img className='newsCard_img' src={urlToImage} />
-        <h4 className='newsCard_title'>{title}</h4>
+        <img className='newsCard_img' src={urlToImage || ''} />
+        <h4 className='newsCard_title'>{title || 'title of the news'}</h4>
       </div>
-      <p className='newsCard_desc'>{description}</p>
+      <p className='newsCard_desc'>{description || 'no description'}</p>
     </a>
   )
 }
