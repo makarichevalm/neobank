8cf952376aab88608224b38ecf195bbecaddd94d
makaricheva liza <makarichevalm@yandex.ru>
2024-12-22 18:19:32 +0300

    add getCard

---
+++ src/components/card/GetCard/GetCard.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+import './GetCard.scss'
+const GetCard: FC = () => {
+  return (
+    <article className='getCard'>
+      <h3>How to get a card</h3>
+      <div className='getCard_step'>
+        <section>
+          <div className='getCard_step_num'>
+            <div className='getCard_step_num-circle'>
+              <span>1</span>
+            </div>
+            <div className='line'></div>
+          </div>
+          <div className='getCard_step_desc'>Fill out an online application - you do not need to visit the bank</div>
+        </section>
+        <section>
+          <div className='getCard_step_num'>
+            <div className='getCard_step_num-circle'>
+              <span>2</span>
+            </div>
+            <div className='line'></div>
+          </div>
+          <div className='getCard_step_desc'>
+            Find out the bank's decision immediately after filling out the application
+          </div>
+        </section>
+        <section>
+          <div className='getCard_step_num'>
+            <div className='getCard_step_num-circle'>
+              <span>3</span>
+            </div>
+            <div className='line'></div>
+          </div>
+          <div className='getCard_step_desc'>
+            The bank will deliver the card free of charge, wherever convenient, to your city
+          </div>
+        </section>
+      </div>
+    </article>
+  )
+}
+
+export default GetCard
--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react'
 import Header from '@components/ui/Header/Header'
 import Footer from '@components/ui/Footer/Footer'
 import PlatinumCard from '@components/card/PlatinumCard/PlatinumCard'
+import GetCard from '@/components/card/GetCard/GetCard'
 
 const CardPage: FC = () => {
   return (
     <div>
       <Header />
       <PlatinumCard />
+      <GetCard />
       <Footer />
     </div>
   )
